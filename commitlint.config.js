56ce2a51b8c088e0df7c51c4200fa18a219c7b53
anilkumarswain140 <anil.swain@mindfiresolutions.com>
2024-11-21 17:51:16 +0530

    fix: Add fixes for commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["fix", "feat", "docs", "style", "refactor", "test", "chore"],
+    ],
+    "type-case": [2, "always", "lower-case"], // Enforce lower-case for types
+    "type-empty": [2, "never"], // Type cannot be empty
+    "subject-empty": [2, "never"], // Subject cannot be empty
+    "subject-case": [2, "always", "sentence-case"], // Enforce sentence case for the subject
+    "header-max-length": [2, "always", 72], // Limit the header to 72 characters
+  },
 };
